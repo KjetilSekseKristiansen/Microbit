1220dc211b3405bed19bdc005900359b58cf8492
KjetilSekseKristiansen <36543031+KjetilSekseKristiansen@users.noreply.github.com>
2018-04-13 11:55:22 +0200

    Update gpiote.c

--- gpiote/gpiote.c
+++ gpiote/gpiote.c
@@ -22,4 +22,7 @@ typedef struct {
 
 int gpiote_surveil_A_button(){
     GPIOTE->CONFIG[0] = (17<<8)| 1 | (2<<16);
+	GPIOTE->CONFIG[1] = (13<<8)| 3 | (1<<16);
+	GPIOTE->CONFIG[2] = (14<<8)| 3 | (1<<16);
+	GPIOTE->CONFIG[3] = (15<<8)| 3 | (1<<16);
 }
